bd0484627138a35b4fb4e26293f06745cb3e1be0
ujjwalsharma515 <125456525+ujjwalsharma515@users.noreply.github.com>
2023-03-09 07:47:20 +0530

    Add files via upload

---
+++ max_element_in_array.java
@@ -0,0 +1,18 @@
+public class max {
+    public static void main(String[] args) {
+        int [] arr = { 45,3,4,56,6,35,5,28,9,453,4555};
+        int max;
+        max = arr[0];
+        for (int i = 0; i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+                
+            }
+           
+        }
+        System.out.println(max);
+
+
+    }
+    
+}
