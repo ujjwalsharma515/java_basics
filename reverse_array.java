f2386e5e8446e0adf52fda15dcdaa096e4240518
ujjwalsharma515 <125456525+ujjwalsharma515@users.noreply.github.com>
2023-03-10 20:16:01 +0530

    Update reverse_array.java

--- reverse_array.java
+++ reverse_array.java
@@ -4,30 +4,26 @@ public class reverse_array {
         int s = arr[0];
         int e = arr[arr.length-1];
   
-    // for(int i = 0;i<=arr.length-1;i++){
-    //     for(int j = arr.length-1 ; j<=0;j--){
-    //         if(i<j){
-    //             arr[i]=arr[j];
 
-    //         }
-            
-    //     }
-      
-        
-       
-    // }
-    // for(int j=0;j<arr.length;j++){
-    //     System.out.print(arr[j]+"  ");
-    // }
+//     for(int i = 0;i<=arr.length-1;i++){
+//         System.out.print(arr[i]+" ");
+//     }
+//     System.out.println();
+// for(int i =arr.length-1;i>=0;i--){
+//     System.out.print(arr[i]+" ");
+        while(i<=j){
+  
+    int temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+    i++;
+    j--;
+}
+for(int k = 0 ; k<arr.length ; k++){
+    System.out.print(arr[k] + " ");
+}
+}
+}
 
-    for(int i = 0;i<=arr.length-1;i++){
-        System.out.print(arr[i]+" ");
-    }
-    System.out.println();
-for(int i =arr.length-1;i>=0;i--){
-    System.out.print(arr[i]+" ");
-}
-}
-}
 
 
